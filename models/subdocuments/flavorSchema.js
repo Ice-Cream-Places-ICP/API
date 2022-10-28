f0ec46e9ab919d31969eb6005442b2820381aa08
Seweryn Szymański <sew.szymanski@gmail.com>
2022-10-29 00:04:36 +0200

    flavors adding bug fix

--- models/subdocuments/flavorSchema.js
+++ models/subdocuments/flavorSchema.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 const flavorSchema = new Schema({
     name: {
         type: String, 
-        unique: true,
-        required: true,
+        required: true
     },
     available: {
         type: Boolean,
