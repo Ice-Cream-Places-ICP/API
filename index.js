73130b3696e979ffff213c4db7e6d8327115084a
B34TER <sew.szymanski@gmail.com>
2022-11-19 15:10:42 +0100

    Implement request logger #24

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
+const requestLogger = require('./middleware/requestLogger');
 
 const authRouts = require('./routes/authRoutes');
 const shopRoutes = require('./routes/shopRoutes.js');
 const app = express();
 
 //MIDDLEWARE
+app.use(requestLogger);
 app.use(express.json());
 app.use(cors());
 app.use('/auth', authRouts);
