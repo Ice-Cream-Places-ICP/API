6a21beeebd5d8ed4b07b1a64a03cffc1fc02517d
B34TER <sew.szymanski@gmail.com>
2022-11-20 18:49:17 +0100

    Modify getShopById to pass tests #28

--- controllers/shopFunctions/getShopById.js
+++ controllers/shopFunctions/getShopById.js
@@ -1,13 +1,20 @@
 const orderComment = require('../../utils/orderComment');
 const sendResponse = require('../../utils/sendResponse');
 const Shop = require('../../models/shopModel.js');
+const mongoose = require('mongoose');
 
 const getShopById = async (req, res) => {
-	orderComment('get by id');
 	try {
-		const shop = await Shop.findById(req.params.shopId);
-		res.json(sendResponse(true, 'Shop info in content', shop));
-		console.log(shop);
+		if (!mongoose.isValidObjectId(req.params.id)) {
+			return res.status(400).json(sendResponse(false, 'Invalid id'));
+		}
+
+		const shop = await Shop.findById(req.params.id);
+		if (!shop) {
+			return res.status(400).json(sendResponse(false, 'Shop not found'));
+		}
+		
+		res.json(sendResponse(true, 'Shop succesfully retrieved', shop));
 	} catch (err) {
 		res.json(sendResponse(false, err));
 	}
