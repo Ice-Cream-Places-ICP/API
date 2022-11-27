63200468ae2dc8be7f319b43864141bad8e22b65
B34TER <sew.szymanski@gmail.com>
2022-11-27 18:58:45 +0100

    Modify constants

--- config/constants.js
+++ config/constants.js
@@ -1,26 +1,15 @@
-const userTypes = {
+const roles = {
     ADMIN: 'admin', 
     DEFAULT: 'default', 
     GUEST: 'guest'
 }
 
-const shopRoles = {
+const jobPositions = {
     OWNER: 'owner',
     EMPLOYEE: 'employee'
 }
 
-const weekDayNames = {
-    0: 'Niedziela', 
-    1: 'Poniedziałek', 
-    2: 'Wtorek', 
-    3: 'Środa', 
-    4: 'Czwartek', 
-    5: 'Piątek', 
-    6: 'Sobota'
-}
-
 module.exports = { 
-    userTypes, 
-    shopRoles, 
-    weekDayNames
+    roles,
+    jobPositions
 };
