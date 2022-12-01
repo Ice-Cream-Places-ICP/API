d37a086e69a7988218eacfd3fb2640ef546659c2
B34TER <sew.szymanski@gmail.com>
2022-12-01 23:22:30 +0100

    Fix verifyRoles middleware

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -2,19 +2,20 @@ const sendResponse = require('../utils/sendResponse');
 const { roles } = require('../config/constants');
 
 const verifyRoles = (...allowedRoles) => {
-    return (req, res, next) => {
-        let permitted = false;
-        allowedRoles.push(roles.ADMIN);
-        req?.user?.roles.forEach(role => {
-            if (allowedRoles.includes(role)) {
-                permitted = true;
-                return next();
-            }
-        })
-        if (!permitted) {
+    return async (req, res, next) => {
+        if (!allowedRoles.includes(roles.ADMIN)) allowedRoles.push(roles.ADMIN);
+
+        const userRoles = req?.user.roles;
+        if (!isPermitted(allowedRoles, userRoles)) {
             return res.status(400).json(sendResponse(false, 'Access denied'));
         }
+        next();
     }
+}
+
+const isPermitted = (allowedRoles, userRoles) => {
+    const intersectionArr = allowedRoles.filter(roles => userRoles.includes(roles));
+    return intersectionArr.length > 0 ? true : false;
 }
 
 module.exports = verifyRoles;
