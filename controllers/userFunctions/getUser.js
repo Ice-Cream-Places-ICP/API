8c2a9454f0ca6607d02df40b1f9188922f84d15b
B34TER <sew.szymanski@gmail.com>
2022-12-02 23:01:37 +0100

    Fix getUser logic

--- controllers/userFunctions/getUser.js
+++ controllers/userFunctions/getUser.js
@@ -6,17 +6,15 @@ const mongoose = require('mongoose');
 const getUser = async (req, res) => {
     const _id = req.params.id;
 
-    if (!_id) {
-        const user = await User.findById(req.user._id).populate('shops.id').select('-password -createdAt -updatedAt -__v').exec();
-
-        return res.status(200).json(sendResponse(true, 'Current user retrieved', user));
+    if (!_id || _id === req.user._id.toString()) {
+        return res.status(200).json(sendResponse(true, 'Current user retrieved', req.user));
     }
 
     if (!mongoose.isValidObjectId(_id)) {
         return res.status(400).json(sendResponse(false, 'Invalid user id'));
     }
 
-    if (isAdmin(req.user) || _id === req.user._id.toString()) {
+    if (isAdmin(req.user)) {
         const user = await User.findById(_id).populate('shops.id').select('-password -createdAt -updatedAt -__v').exec();
         if (!user) {
             return res.status(400).json(sendResposne(false, 'User not found'));
