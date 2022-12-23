a30b1a7ffa6c70487f1e68439fd67a86912814de
B34TER <sew.szymanski@gmail.com>
2022-12-23 23:27:55 +0100

    Add getAllUsers endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,18 @@ const verifyToken = require('../middleware/verifyToken');
 const getUserInfo = require('../middleware/getUserInfo');
 const getUser = require('../controllers/userFunctions/getUser');
 const updateUser = require('../controllers/userFunctions/updateUser');
+const getAllUsers = require('../controllers/userFunctions/getAllUsers');
+const verifyRoles = require('../middleware/verifyRoles');
+const { roles } = require('../config/constants');
 
 router.use(verifyToken);
 router.use(getUserInfo);
+
+router.get(
+    '/all',
+    verifyRoles(roles.ADMIN),
+    getAllUsers
+)
 
 router.get(
     '/:id?',
