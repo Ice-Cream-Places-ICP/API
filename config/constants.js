c1ea83f9c2310c2891d735ba379317ead55c9ef3
Adrian Rudnicki <adi2602@wp.pl>
2022-12-29 23:04:03 +0100

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,22 +1,22 @@
 const roles = {
-    ADMIN: 'admin', 
-    DEFAULT: 'default', 
-    OWNER: 'owner',
-    EMPLOYEE: 'employee'
-}
+  ADMIN: "admin",
+  DEFAULT: "default",
+  OWNER: "owner",
+  EMPLOYEE: "employee",
+};
 
 const userStatus = {
-    PENDING: 'pending',
-    ACTIVE: 'active',
-}
+  PENDING: "pending",
+  ACTIVE: "active",
+};
 
 const allowedOrigins = [
-    `http://localhost:${process.env.WEB_PORT}`,
-    'https://ice-cream-places-web.vercel.app/'
-]
+  `http://localhost:${process.env.WEB_PORT}`,
+  "https://ice-cream-places-web.vercel.app",
+];
 
-module.exports = { 
-    roles,
-    allowedOrigins,
-    userStatus
+module.exports = {
+  roles,
+  allowedOrigins,
+  userStatus,
 };
