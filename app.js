c89790e7e9f06d20d048395e88d092589812bce7
B34TER <sew.szymanski@gmail.com>
2022-12-30 12:34:17 +0100

    Rename index.js and server.js files, move code

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");
+const requestLogger = require("./middleware/requestLogger");
+
+const corsOptions = require("./config/corsOptions");
+const authRouts = require("./routes/authRoutes");
+const shopRoutes = require("./routes/shopRoutes.js");
+const userRoutes = require("./routes/userRoutes.js");
+const app = express();
+
+//MIDDLEWARE
+app.use(requestLogger);
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use("/auth", authRouts);
+app.use("/shops", shopRoutes);
+app.use("/users", userRoutes);
+app.use(errorHandler);
+
+module.exports = app;
