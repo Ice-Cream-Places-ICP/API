aa2e5742a192b876f3c6aec83c93dad0f8f538c3
B34TER <sew.szymanski@gmail.com>
2023-01-04 21:06:04 +0100

    Add google-oauth20 passport strategy

---
+++ config/passport-setup.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const User = require('../models/User');
+const { authMethod, userStatus } = require('./constants');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    })
+})
+
+passport.use(
+    new GoogleStrategy({
+        callbackURL: '/auth/google/redirect',
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }, async (accessToken, refreshToken, profile, done) => {
+        let user = await User.findOne({ googleId: profile.id }).exec();
+
+        if (!user) {
+            const email = profile._json.email;
+            const googleId = profile.id;
+            const authType = authMethod.GOOGLE;
+            const status = userStatus.ACTIVE;
+
+            user = new User({
+                email,
+                googleId,
+                authType,
+                status
+            });
+
+            user = await user.save();
+        }
+        else {
+            if (user.authType !== authMethod.GOOGLE) {
+                const err = new Error('This account can only be logged into with Google');
+                return done(err);
+            }
+        }
+
+        done(null, user);
+    })
+)
