480d6d43b032f28cf35adcd28becb0fa4fdc6f5f
Adrian Rudnicki <92043491+adiko2602@users.noreply.github.com>
2023-01-06 00:28:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const app = express();
 app.use(requestLogger);
 app.use(cookieSession({
     maxAge: 5 * 60 * 60 * 1000,
-    keys: [process.env.TOKEN_SECRET]
+    keys: [process.env.TOKEN_SECRET],
+    httpOnly: false,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
