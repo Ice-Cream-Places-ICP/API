f0d7aee14e69c225162a76ac1116571a834a119f
Adrian Rudnicki <adi2602@wp.pl>
2023-01-16 12:34:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 const requestLogger = require("./middleware/requestLogger");
-const passportSetup = require('./config/passport-setup');
+const passportSetup = require("./config/passport-setup");
 const corsOptions = require("./config/corsOptions");
 const authRouts = require("./routes/authRoutes");
 const shopRoutes = require("./routes/shopRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
-const mailRoutes = require('./routes/mailRoutes');
+const mailRoutes = require("./routes/mailRoutes");
 const app = express();
 
 //MIDDLEWARE
@@ -20,5 +20,7 @@ app.use("/shops", shopRoutes);
 app.use("/users", userRoutes);
 app.use("/mail", mailRoutes);
 app.use(errorHandler);
+
+// test for CI
 
 module.exports = app;
